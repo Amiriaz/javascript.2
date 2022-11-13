bd79bc659ef2ee31953a1f2eeb48bd0de0601c04
Amir Riaz <116000783+Amiriaz@users.noreply.github.com>
2022-11-14 00:42:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,222 @@
+// var spec = prompt("human");
+// console.log(spec);
+
+// var question = "Your species?";
+//  var defaultAnswer = "human";
+//  var spec = prompt(question, defaultAnswer);
+// console.log(question + defaultAnswer + spec);
+
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
+// console.log(numberOfCats =tooManyCats = numberOfCats);
+
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = true;
+
+// if(correctAnswer === true){  
+//     console.log("Vatican");
+// }
+
+//  var correctAnswer = true;
+//  var userIQ = true;
+//   if (correctAnswer === true) {
+  
+//  console.log("Vatican");
+//  console.log("genius");
+//   }
+
+// var fullName = true;
+// if (fullName ===  true) {
+// console.log("Mark" + "  " + "Myers");
+// }
+
+// var fullName;
+// var firstName;
+// if (fullName === "firstName + " + "lastName") {
+// console.log(fullName = firstName);
+// }
+// var x,y,a,b;
+//     if ( "x + y === a - b") {
+//         console.log(x = y = a - b);
+//     }
+// var yourTicketNumber;
+//    if (yourTicketNumber !== 487208) {
+//     console.log("Better luck next time.");
+//                   }
+
+// var x = true;
+// var Vatican = true;
+// if (x === Vatican) {
+//     console.log("Correct!");
+//      }
+//      else {
+//         console.log("Wrong answer");
+//      }
+
+    //  var x = true;
+    //  var userIQ = false;
+    //  if (x === userIQ) {
+    //      console.log("Correct!");
+    //       }
+    //       else {
+            
+    //         console.log("problematic");  
+    //          console.log("Incorrect");
+    //       }
+  
+// var correctAnswer = true;
+// var x = true;
+// if (x === false) {
+//     console.log("Correct!");
+// }
+// else if (x === true) {
+// console.log("Incorrect but close");
+// }
+// else {
+// console.log("Incorrect");
+//  }
+
+// var weight = false;
+// var time = true;
+// if (weight > 300 && time < 6) {
+//     console.log("Come to our tryout!");
+//      }
+//      else {
+//         console.log("Come to our cookout!");
+//      }
+
+// var c = d = x = y = true;
+// var a = b = true;
+
+// if (c === d) { 
+//     console.log(c === d),(x === y),(a === b);
+// }
+// else {
+//     console.log( g = h = e = f);
+//     }
+//  if (x === y) {
+
+//  }
+//  else if (a === b) {
+//     console.log(g = h = e = f);
+//  }
+//  else {
+//  console.log( g = h = e = f);
+//  }
+ 
+
+// var x = y = a = b = true;
+// var c = d = true ;
+// if ((x === y || a === b) && c === d) {
+// console.log((x === y || a === b) && c === d);
+//  }
+//  else {
+// console.log( g = h = e = f);
+//     }
+
+
+// user input
+
+// var ab = prompt()
+
+// console.log(ab)
+
+// var FirstName=prompt("enter first name:")
+// var LastName=prompt("enter last name")
+// var city=prompt("Enter your City","karachi")
+// console.log(FirstName)
+// console.log(LastName)
+// console.log(city)
+
+
+// percentage program
+
+// var eng = +prompt("Enter the marks of English")
+// var phy = +prompt("Enter the marks of Physics")
+// var maths = +prompt("Enter the marks of Maths")
+// var discrete = +prompt("Enter the marks of Discrete")
+// var dsa = +prompt("Enter the marks of Data Structure")
+
+// console.log("Your English Marks are= "+(eng))
+// console.log(phy)
+// console.log(maths)
+// console.log(discrete)
+// console.log(dsa)
+
+// var total= eng+phy+maths+discrete+dsa;
+// console.log("your total marks"+(total))
+
+// var per=(total/500)*100
+
+// console.log("your percentage="+(per))
+
+
+// if(per>=70)
+// {
+//     console.log("A grade")
+// }
+
+// else if(per>=60)
+// {
+//     console.log("B grade")
+// }
+
+// else{
+//     console.log("fail")
+// }
+
+
+
+// conditions
+
+// var officer = false
+// var assi= false
+
+// if(officer==true)
+// {
+//     console.log("documents are submitted!")
+// }
+
+
+// else if(assi==true)
+// {
+//     console.log("documents are submitted to Assistant")
+// }
+
+
+// else{
+//     console.log("documents can not be submitted!")
+// }
+
+
+
+
+// ==(equal)
+// !=(not equal)
+// ===(equal with data type)
+// !==(not equal with data type)
+
+
+var age=15
+var studentcard=true
+
+if(age==16){
+    console.log("Allow")
+}
+
+else{
+
+    if(studentcard==true)
+    {
+        console.log("Allow on student car")
+    }
+
+    else if(studentcard==true && age==15)
+    {
+        console.log("allow")
+    }
+
+    else{
+    console.log("not Allow")
+    }  
+}
